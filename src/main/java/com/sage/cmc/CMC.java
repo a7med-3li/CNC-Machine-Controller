326eb13116b8b5767e187fe5d346f176bba5150e
a7med-3li <154826599+a7med-3li@users.noreply.github.com>
2024-12-23 07:10:30 +0200

    Adding main class to start the app

---
+++ src/main/java/com/sage/cmc/CMC.java
@@ -0,0 +1,16 @@
+package com.sage.cmc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CMC extends Application{
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
